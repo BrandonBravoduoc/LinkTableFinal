5e1eab5b0292dcff453be9aac3da2c67cb968198
BenjaminMarin4444 <be.marinv@duocuc.cl>
2025-06-21 21:24:27 -0400

    GSGFDGDF
    FDSFD

--- src/main/java/com/LinkTable/LinkTable/controller/AuthController.java
+++ src/main/java/com/LinkTable/LinkTable/controller/AuthController.java
@@ -1,16 +1,18 @@
 
 package com.LinkTable.LinkTable.controller;
 
-
 import com.LinkTable.LinkTable.model.LoginRequest;
 import com.LinkTable.LinkTable.service.AuthService;
 
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
 import io.swagger.v3.oas.annotations.tags.Tag;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("api/v1/auth")
--- src/main/java/com/LinkTable/LinkTable/controller/v2/HistorialSesionControllerV2.java
+++ src/main/java/com/LinkTable/LinkTable/controller/v2/HistorialSesionControllerV2.java
@@ -5,7 +5,7 @@ import com.LinkTable.LinkTable.model.HistorialSesion;
 import com.LinkTable.LinkTable.service.HistorialSesionService;
 
 import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.parameters.RequestBody;
+
 import io.swagger.v3.oas.annotations.tags.Tag;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,6 +17,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -65,10 +66,11 @@ public class HistorialSesionControllerV2 {
     @PostMapping(produces = MediaTypes.HAL_JSON_VALUE)
     @Operation(summary = "Esta api se encarga de crear historial de inicio de sesión", description = "Esta api se encarga de crear un historial de inicio de sesión, incluyendo enlaces HATEOAS")
     public ResponseEntity<EntityModel<HistorialSesion>> createHistorialSesion(
-        @RequestBody HistorialSesion historialSesion) {
+            @RequestBody HistorialSesion historialSesion) {
         HistorialSesion newSesion = historialSesionService.save(historialSesion);
         return ResponseEntity
-                .created(linkTo(methodOn(HistorialSesionControllerV2.class).getHistorialSesionById(Long.valueOf(newSesion.getId()))).toUri())
+                .created(linkTo(methodOn(HistorialSesionControllerV2.class)
+                        .getHistorialSesionById(Long.valueOf(newSesion.getId()))).toUri())
                 .body(assembler.toModel(newSesion));
     }
 
