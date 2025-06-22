3d8d34e8d54f66b5b32b763e07e7235e94a64a16
BrandonBravoduoc <bra.bravol@duocuc.cl>
2025-06-21 23:47:34 -0400

    asd

--- src/main/java/com/LinkTable/LinkTable/repository/UsuarioRepository.java
+++ src/main/java/com/LinkTable/LinkTable/repository/UsuarioRepository.java
@@ -1,8 +1,11 @@
 package com.LinkTable.LinkTable.repository;
 
+
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.LinkTable.LinkTable.model.Usuario;
 
@@ -10,4 +13,8 @@ import com.LinkTable.LinkTable.model.Usuario;
 public interface UsuarioRepository extends JpaRepository<Usuario, Long>{
 
     Optional<Usuario> findByCorreo(String correo);    
+
+    @Query("SELECT u FROM Usuario u WHERE u.correo = :correo")
+    Usuario obtenerUsuarioPorCorreo(@Param("correo") String correo);
+
 }
